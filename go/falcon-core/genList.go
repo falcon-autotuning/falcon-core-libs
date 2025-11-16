c78d2264c58fcfffa6e39907ac40d8c08f592856
tjk144 <tkovach@wisc.edu>
2025-11-16 13:28:43 -0600

    Complete test generation for the list. NOw just need to grind out all the types

--- go/falcon-core/genList.go
+++ go/falcon-core/genList.go
@@ -20,17 +20,14 @@ type ListType struct {
 	OptTestDefaultListData string // e.g. "{1, 2, 3}". S
 	OptTestVal1            string // e.g. "42"
 	OptTestOtherListData   string // e.g. "{99}"
+	CElemTypeConstructor   string // only needed for non PrimitiveType
 }
 
-// If PrimitiveType is selected then the OptTestDefaultListData, OptTestVal1, OptDefaultElemType and the OptTestDefaultListData do absolutely nothing and the user should implement these in a testdata.go file in teh folder of the tests that are not primitive
-
 func toPkgName(typeName string) string {
-	// Example: ListInt -> listInt, ListFloat -> listFloat
 	if typeName == "" {
 		return ""
 	}
 	runes := []rune(typeName)
-	// Lowercase the first word
 	i := 1
 	for ; i < len(runes); i++ {
 		if runes[i] >= 'A' && runes[i] <= 'Z' {
@@ -47,20 +44,18 @@ func main() {
 		{Type: "ListDouble", ElemType: "float64", CType: "double", Header: "ListDouble_c_api.h", OptDefaultElemType: "0.0", OptTestDefaultListData: "{1.1,2.2}", OptTestVal1: "3.3", OptTestOtherListData: "{1.0}", PrimitiveType: true},
 		{Type: "ListBool", ElemType: "bool", CType: "bool", Header: "ListBool_c_api.h", OptDefaultElemType: "false", OptTestDefaultListData: "{true,false}", OptTestVal1: "true", OptTestOtherListData: "{true}", PrimitiveType: true},
 		{Type: "ListSizeT", ElemType: "uint64", CType: "size_t", Header: "ListSizeT_c_api.h", OptDefaultElemType: "0", OptTestDefaultListData: "{0,1}", OptTestVal1: "4", OptTestOtherListData: "{3}", PrimitiveType: true},
-		{Type: "ListConnection", ElemType: "*connection.Handle", CType: "ConnectionHandle", Header: "ListConnection_c_api.h", OptionalImport: `"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/deviceStructures/connection"`, PrimitiveType: false},
+		{Type: "ListConnection", ElemType: "*connection.Handle", CType: "ConnectionHandle", Header: "ListConnection_c_api.h", OptionalImport: `"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/deviceStructures/connection"`, PrimitiveType: false, CElemTypeConstructor: "connection.FromCAPI"},
 		// Add more types here...
 	}
+	// Parse as separate templates, no {{define ...}} in files
+	wrapperTmpl := template.Must(template.ParseFiles("generic/list/list_wrapper.tmpl"))
+	testTmpl := template.Must(template.ParseFiles("generic/list/list_test_wrapper.tmpl"))
 
-	tmpl := template.Must(template.ParseFiles(
-		"generic/list/list_wrapper.tmpl",
-		"generic/list/list_test_wrapper.tmpl",
-	))
 	manifest, err := os.Create("list_handles_manifest.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer manifest.Close()
-
 	for _, t := range types {
 		pkg := toPkgName(t.Type)
 		t.Package = pkg
@@ -74,7 +69,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		if err := tmpl.ExecuteTemplate(f, "list_wrapper.tmpl", t); err != nil {
+		if err := wrapperTmpl.Execute(f, t); err != nil {
 			panic(err)
 		}
 		f.Close()
@@ -86,7 +81,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		if err := tmpl.ExecuteTemplate(tf, "list_wrapper_test.tmpl", t); err != nil {
+		if err := testTmpl.Execute(tf, t); err != nil {
 			panic(err)
 		}
 		tf.Close()
