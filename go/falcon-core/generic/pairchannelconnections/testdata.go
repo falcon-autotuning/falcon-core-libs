42197424e1c18be13bb8d3685f893ea9cdf999c2
tjk144 <tkovach@wisc.edu>
2025-11-17 13:16:50 -0600

    Most pairs are now supported that are lowest on the food chain for the go wrapper

---
+++ go/falcon-core/generic/pairchannelconnections/testdata.go
@@ -0,0 +1,47 @@
+package pairchannelconnections
+
+import (
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/autotuner-interfaces/names/channel"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/deviceStructures/connection"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/deviceStructures/connections"
+)
+
+func mustBarrierGate(name string) *connection.Handle {
+	h, err := connection.NewBarrierGate(name)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustChannel(name string) *channel.Handle {
+	h, err := channel.New(name)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustGates(names ...string) []*connection.Handle {
+	var handles []*connection.Handle
+	for _, n := range names {
+		handles = append(handles, mustBarrierGate(n))
+	}
+	return handles
+}
+
+func mustConnections(names ...string) *connections.Handle {
+	handles := mustGates(names...)
+	h, err := connections.New(handles)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+var (
+	defaultChannel     = mustChannel("C1")
+	defaultConnections = mustConnections("B4")
+	otherChannel       = mustChannel("C2")
+	otherConnections   = mustConnections("B3")
+)
---
+++ go/falcon-core/generic/pairconnectionpairquantityquantity/testdata.go
@@ -0,0 +1,52 @@
+package pairconnectionpairquantityquantity
+
+import (
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/generic/pairquantityquantity"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/math/quantity"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/deviceStructures/connection"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/units/symbolunit"
+)
+
+func mustBarrierGate(name string) *connection.Handle {
+	h, err := connection.NewBarrierGate(name)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustVolt() *symbolunit.Handle {
+	h, err := symbolunit.NewVolt()
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustQuantity(val float64) *quantity.Handle {
+	h, err := quantity.New(val, mustVolt())
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustPairQuantityQuantity(vals ...float64) *pairquantityquantity.Handle {
+	if len(vals) != 2 {
+		panic("mustPairQuantityQuantity requires exactly 2 values")
+	}
+	q1 := mustQuantity(vals[0])
+	q2 := mustQuantity(vals[1])
+	h, err := pairquantityquantity.New(q1, q2)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+var (
+	defaultConnection           = mustBarrierGate("B1")
+	defaultPairQuantityQuantity = mustPairQuantityQuantity(1.0, 2.2)
+	otherConnection             = mustBarrierGate("B2")
+	otherPairQuantityQuantity   = mustPairQuantityQuantity(5.4, 4.3)
+)
