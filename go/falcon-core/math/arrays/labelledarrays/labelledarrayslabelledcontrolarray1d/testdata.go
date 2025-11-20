4475161a1bfa6e92b7c707eddc79faab8de430dd
tjk144 <tkovach@wisc.edu>
2025-11-19 19:37:26 -0600

    Labelled Arrays are now supported by go wrapper

---
+++ go/falcon-core/math/arrays/labelledarrays/labelledarrayslabelledcontrolarray1d/testdata.go
@@ -0,0 +1,68 @@
+package labelledarrayslabelledcontrolarray1d
+
+import (
+	"fmt"
+
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/autotuner-interfaces/contexts/acquisitioncontext"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/generic/farraydouble"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/instrument-interfaces/names/instrumentport"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/instrument-interfaces/names/instrumenttypes"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/math/arrays/labelledcontrolarray1d"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/deviceStructures/connection"
+	"github.com/falcon-autotuning/falcon-core-libs/go/falcon-core/physics/units/symbolunit"
+)
+
+func mustAcquisitionContext(port *instrumentport.Handle) *acquisitioncontext.Handle {
+	h, err := acquisitioncontext.NewFromPort(port)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustVolt() *symbolunit.Handle {
+	h, err := symbolunit.NewVolt()
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func mustBarrierGate(name string) *connection.Handle {
+	h, err := connection.NewBarrierGate(name)
+	if err != nil {
+		panic(fmt.Errorf("failed to create BarrierGate: %v", err))
+	}
+	return h
+}
+
+func mustInstrumentPort(name string, conn *connection.Handle, insttype string, unit *symbolunit.Handle, desc string) *instrumentport.Handle {
+	h, err := instrumentport.NewKnob(name, conn, insttype, unit, desc)
+	if err != nil {
+		panic(fmt.Errorf("failed to create Knob: %v", err))
+	}
+	return h
+}
+
+func mustControlArray(data []float64, shape []int, name string) *labelledcontrolarray1d.Handle {
+	f, err := farraydouble.FromData(data, shape)
+	if err != nil {
+		panic(err)
+	}
+	ac := mustAcquisitionContext(mustInstrumentPort(name, mustBarrierGate(name), instrumenttypes.VoltageSource(), mustVolt(), ""))
+	h, err := labelledcontrolarray1d.FromFArray(f, ac)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+var (
+	defaultListData = []*labelledcontrolarray1d.Handle{
+		mustControlArray([]float64{0.0, 1.0}, []int{2}, "B1"),
+		mustControlArray([]float64{0.0, 1.0, 2.0, 3.0}, []int{4}, "B2"),
+	}
+	otherListData = []*labelledcontrolarray1d.Handle{
+		mustControlArray([]float64{0.5, 1.0, 2.0, 3.0}, []int{4}, "B3"),
+	}
+)
---
+++ go/falcon-core/tools/genLabelledArrays/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+type ArrayType struct {
+	Package     string
+	File        string
+	Header      string
+	Type        string
+	CArrayType  string
+	GoArrayType string
+}
+
+func toFileName(typeName string) string {
+	if typeName == "" {
+		return ""
+	}
+	runes := []rune(typeName)
+	i := 1
+	for ; i < len(runes); i++ {
+		if runes[i] >= 'A' && runes[i] <= 'Z' {
+			break
+		}
+	}
+	return strings.ToLower(string(runes[:i])) + string(runes[i:])
+}
+
+func toPkgName(typeName string) string {
+	if typeName == "" {
+		return ""
+	}
+	return strings.ToLower(typeName)
+}
+
+func main() {
+	types := []ArrayType{
+		{
+			Type:       "LabelledArraysLabelledControlArray1D",
+			CArrayType: "LabelledControlArray1D",
+		},
+		{
+			Type:       "LabelledArraysLabelledControlArray",
+			CArrayType: "LabelledControlArray",
+		},
+		{
+			Type:       "LabelledArraysLabelledMeasuredArray1D",
+			CArrayType: "LabelledMeasuredArray1D",
+		},
+		{
+			Type:       "LabelledArraysLabelledMeasuredArray",
+			CArrayType: "LabelledMeasuredArray",
+		},
+	}
+	for i := range types {
+		types[i].Header = types[i].Type + "_c_api.h"
+		types[i].Package = toPkgName(types[i].Type)
+		types[i].File = toFileName(types[i].Type)
+		types[i].GoArrayType = toPkgName(types[i].CArrayType)
+	}
+	wrapperTmpl := template.Must(template.ParseFiles("math/arrays/labelledarrays/labelledArrays_wrapper.tmpl"))
+	testTmpl := template.Must(template.ParseFiles("math/arrays/labelledarrays/labelledArrays_test_wrapper.tmpl"))
+	manifest, err := os.Create("labelledarrays_handles_manifest.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer manifest.Close()
+	for _, t := range types {
+		dir := filepath.Join("math", "arrays", "labelledarrays", t.Package)
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			panic(err)
+		}
+		implFile := filepath.Join(dir, t.File+".go")
+		f, err := os.Create(implFile)
+		if err != nil {
+			panic(err)
+		}
+		if err := wrapperTmpl.Execute(f, t); err != nil {
+			panic(err)
+		}
+		f.Close()
+		fmt.Fprintln(manifest, implFile)
+		testFile := filepath.Join(dir, t.File+"_test.go")
+		tf, err := os.Create(testFile)
+		if err != nil {
+			panic(err)
+		}
+		if err := testTmpl.Execute(tf, t); err != nil {
+			panic(err)
+		}
+		tf.Close()
+		fmt.Fprintln(manifest, testFile)
+	}
+}
